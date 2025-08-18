2192049e4b313aff69710d2366f8f27bfcd2b05a
Parag Jain <jainparag00@gmail.com>
2025-08-18 19:02:28 +0530

    add repository explorer basic code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RepositoryList from './components/RepositoryList';
+import RepositoryDetails from './components/RepositoryDetails';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <header className="header">
+          <h1>GoDaddy Repositories</h1>
+          <p>Explore GoDaddy's open source projects</p>
+        </header>
+        
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<RepositoryList />} />
+            <Route path="/repo/:owner/:name" element={<RepositoryDetails />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
